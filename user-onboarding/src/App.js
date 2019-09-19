24a501dc2db18d51767c9288b50333f72ce2c461
Funmi7 <talabifunmilayo@gmail.com>
2019-09-19 15:36:21 +0100

    Created a form with the use of formik

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserForm from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const userAPI = 'https://reqres.in/api/users';
+
+// const initialUserForm = {
+//   name: '',
+//   email: '',
+//   password: '',
+//   checkbox: ''
+// }
+
+// function App() {
+//   const [usersList setUsersList] = usestate([])
+//   return (
+//     <div className="App">
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+function UserForm() {
+  <Formik 
+    render={props => {
+      return (
+        <Form>
+          <div>
+            <label>
+              Name
+              <Field name='name' type='text' placeholder='Name'/>
+              <ErrorMessage name='name' component='div'/>
+            </label>
+          </div>
+          <div>
+            <label>
+              Email
+              <Field name='email' type='email' placeholder='Email'/>
+              <ErrorMessage name='email' component='div'/>
+            </label>
+          </div>
+          <div>
+            <label>
+              Password
+              <Field name='password' type='password' placeholder='Password'/>
+              <ErrorMessage name='password' component='div'/>
+            </label>
+          </div>
+          <div>
+            <label>
+              Terms of Service
+              <Field name='terms-of-service' type='checkbox' placeholder='TermsOfService'/>
+              <ErrorMessage name='terms-of-servie' component='div'/>
+            </label>
+          </div>
+          <button type='submit'>Submit</button>
+        </Form>
+    )
+}} 
+/>
+}
+
+export default UserForm;
